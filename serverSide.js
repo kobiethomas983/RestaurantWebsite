1cbffe18937a5b30548c0623b01fa7755d7ca0ae
kobiethomas983 <kobiethomas983@gmail.com>
2019-01-06 14:09:50 -0500

    add files via upload

---
+++ serverSide.js
@@ -0,0 +1,22 @@
+//needed to use the app.listen methods and app.use method
+var express = require("express");
+var app = express();
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var portno = 9001;
+
+app.use(bodyParser.urlencoded({extended:true}));
+//logger variable logs the get methods being used by client
+app.use(logger("dev"));
+//look into the website folder to check for sources for the routes/path
+app.use(express.static("website"));
+
+//if they use a type a route/path we do not recognize send the 404 error
+  app.use((req, res, next) => {
+  res.send("404 ERROR").sendStatus(404);
+});
+
+//listen on the specfic portnumber 9001
+app.listen(portno, function(){
+  console.log("The server has started on port 9001");
+});
